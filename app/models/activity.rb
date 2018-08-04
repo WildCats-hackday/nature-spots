0a9c0f6a6b4f8a59352c9ddd06e4017320fbb008
Pedro Neto <pedro.neto@bemind.com.br>
2018-08-04 20:37:43 -0300

    add relacionamentos

--- app/models/activity.rb
+++ app/models/activity.rb
@@ -1,2 +1,4 @@
 class Activity < ApplicationRecord
+    has_many :events
+    has_many :places, :through => :events
 end
--- app/models/place.rb
+++ app/models/place.rb
@@ -1,2 +1,4 @@
 class Place < ApplicationRecord
+    has_many :events
+    has_many :activities, :through => :events
 end
